4eb4400f489c4c74e3a237f5c7aa559b52c45e2c
John Else <john.else@citrix.com>
2013-11-08 01:06:42 +0000

    Implement simple logging bot
    Signed-off-by: John Else <john.else@citrix.com>

--- ring3bot/ring3bot.ml
+++ ring3bot/ring3bot.ml
@@ -1,2 +1,45 @@
+module C = Irc_client_lwt.Client
+open Lwt
+
+let string_opt_to_string = function
+  | Some s -> s
+  | None -> "None"
+
+let string_list_to_string string_list =
+  Printf.sprintf "[%s]" (String.concat "; " string_list)
+
+let print_message {Irc_message.prefix; command; params; trail} =
+  Lwt_io.printf "Got message: %s %s %s %s\n"
+    (string_opt_to_string prefix)
+    command
+    (string_list_to_string params)
+    (string_opt_to_string trail)
+
+let callback =
+  let open Irc_message in function
+  | Message contents ->
+    print_message contents
+  | Parse_error (message, error) ->
+    Lwt_io.printlf "Couldn't parse \"%s\": %s" message error
+
+let lwt_main config =
+  let open Config_t in
+  C.connect
+    ~server:config.server
+    ~port:config.port
+    ~username:(match config.username with
+      | Some username -> username
+      | None -> config.nick)
+    ~mode:0
+    ~realname:(match config.realname with
+      | Some realname -> realname
+      | None -> config.nick)
+    ~nick:config.nick
+    ~password:config.password
+  >>= (fun connection ->
+    C.send_join ~connection ~channel:config.channel
+    >>= (fun () -> C.listen ~connection ~callback))
+
 let () =
-	print_endline "done"
+  let config = Config.load () in
+  Lwt_main.run (lwt_main config)
